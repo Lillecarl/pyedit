be1574ac022a25c053c06c40f5573a1ce6d31c99
lillecarl <carl.andersson@dynamist.se>
2026-09-11 16:18:13 +0200

    Add nix devshell with pyedit installed
    Assisted-By: glm-5.3-flash

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-{
+rec {
   pyedit = pkgs.callPackage ./pyedit { };
+  shell = pkgs.mkShell {
+    packages = [ (pkgs.python3.withPackages (_: pyedit)) ];
+  };
 }
